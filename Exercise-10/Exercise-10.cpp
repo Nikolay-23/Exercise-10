9bdfe6da6f6b7e016d22dec154b44243d53e1d3a
4KT <4KT@DESKTOP-J0KOTS8>
2024-11-11 03:04:02 +0000

    Add project files.

---
+++ Exercise-10/Exercise-10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+    int main() 
+    {
+        for (int i = 1; i <= 17; i++) 
+        {
+        
+            if (i % 2 != 0) {
+                printf("%d ", i); 
+            }
+        }
+        return 0;
+    }
+
+
